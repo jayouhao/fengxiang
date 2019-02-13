a43e73c8a1a7851942f8f56f072fc412de8e3e34
hh1214hh <jayouhao>
2019-02-13 21:47:05 +0800

    2

--- API-police/API-police.js
+++ API-police/API-police.js
@@ -3,6 +3,7 @@ $(function(){
     $(".report").click(function () {
         $(".reporte8").removeClass("reporte8");
         $(this).addClass("reporte8");
+        $("."+$(this).attr("id")).show().siblings().hide();
         // if($(this).text()=="报警信息"){
         //     $(".centen-police-set").hide();
         //     $(".centen-police-message").show();
@@ -15,13 +16,31 @@ $(function(){
     $(".prot-type").click(function(){
         $(".prot-type1").removeClass("prot-type1");
         $(this).addClass("prot-type1");
+    })        
+    //选择监控类型
+    $(".replaver-api").on('click', 'a', function () {
+        $(".select-type1").text($(this).find("label").text())
     })
-    //个人认证资料
-    $(".remove-x").click(function(){
-        $(".popup-recharge").hide();
+    //选择监控类型，查询的
+    $(".record-type").on('click', 'a', function () {
+        $(".select-type2").text($(this).find("label").text())
     })
-    //提交
-    $(".prot-submit").click(function(){
-        $(".popup-recharge").show();
-    })
+    //分页信息的
+    $('#paging').jqPaginator({
+        //总页数
+        totalPages: 100,
+        //显示多少个页码
+        visiblePages: 5,
+        //当前页
+        currentPage: 1,
+        first: '<li class="first"><a href="javascript:void(0);">首页<\/a><\/li>',
+        prev: '<li class="prev"><a href="javascript:void(0);"><i class="arrow arrow2"><\/i>上一页<\/a><\/li>',
+        next: '<li class="next"><a href="javascript:void(0);">下一页<i class="arrow arrow3"><\/i><\/a><\/li>',
+        last: '<li class="last"><a href="javascript:void(0);">末页<\/a><\/li>',
+        page: '<li class="page"><a href="javascript:void(0);">{{page}}<\/a><\/li>',
+        onPageChange: function (n) {
+            $("#demo2-text").html("当前第" + n + "页");
+            console.log("你选择的=>", n, "页");
+        }
+    });
 })
--- user-setting/user-setting.js
+++ user-setting/user-setting.js
@@ -3,4 +3,32 @@ $(function(){
     $(".setting-submit").click(function(){
         window.history.back(-1);
     })
+    //个人认证资料
+    $(".remove-x").click(function(){
+        $(".popup-recharge").hide();
+    })
+    //个人信息
+    $(".message-remove-x").click(function(){
+        $(".popup-message").hide();
+    })
+    //企业信息
+    $(".firm-message-remove").click(function(){
+        $(".firm-message").hide();
+    })        
+    //提示的    
+    $(".hint-amend").click(function(){
+        $(".popup-hint").hide();
+    })
+    //企业提示的    
+    $(".firm-amend").click(function(){
+        $(".firm-hint").hide();
+    })    
+    //企业认证的
+    $(".firm-remove-x").click(function(){
+        $(".firm-recharge").hide();
+    })
+    //提交
+    // $(".prot-submit").click(function(){
+    //     $(".popup-recharge").show();
+    // })
 })
